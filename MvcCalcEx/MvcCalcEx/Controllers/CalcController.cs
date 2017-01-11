89e32c9003cb957071b2343d7d903cd4c0422d09
simon <simon@glashoff.dk>
2017-01-11 13:12:16 +0100

    Simple calculater

---
+++ MvcCalcEx/MvcCalcEx/Controllers/CalcController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCalcEx.Controllers
+{
+    public class CalcController : Controller
+    {
+        // GET: Calc
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Models.CalcModel model = new Models.CalcModel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Index(string submit, Models.CalcModel model)
+        {
+            int res;
+
+            switch (submit)
+            {
+                case "add":
+                    res = model.Num1 + model.Num2;
+                    break;
+                case "subtract":
+                    res = model.Num1 - model.Num2;
+                    break;
+                case "divide":
+                    res = model.Num1 / model.Num2;
+                    break;
+                case "multiply":
+                    res = model.Num1 * model.Num2;
+                    break;
+                default:
+                    return new HttpStatusCodeResult
+                        (System.Net.HttpStatusCode.NotImplemented,
+                        "calcControler not implemented " + submit);
+            }
+            model.Result = res;
+            
+            //ModelState.Clear(); //ville fjerne indholdet i alle tekstboxe
+            ModelState.Remove("Result"); //Clearer den bestemte tekstbox
+            return View(model); //sender de nye værdier til viewet.
+        }
+    }
+}
